b59751ff6d80aa2a4251b6da4e63a19852bbe92d
dinhnguyen28 <dinh.nguyen2842000@gmail.com>
2022-09-20 11:01:46 +0700

    nhấn vào listTitle thay đổi icon

--- lib/main.dart
+++ lib/main.dart
@@ -56,8 +56,17 @@ class _RandomWordsState extends State<RandomWords> {
           ),
           trailing: Icon(
             alreadySaved ? Icons.favorite : Icons.favorite_border,
-            color: alreadySaved ? Colors.red : null,
+            color: alreadySaved ? Colors.pinkAccent : null,
           ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestions[index]);
+              } else {
+                _saved.add(_suggestions[index]);
+              }
+            });
+          },
         );
       },
     );
